473ccc5a54bc7f9f8b2a53d9549881904a437042
NourElDinN <47853447+NourElDinN@users.noreply.github.com>
2019-02-22 11:44:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,71 +16,65 @@ client.on('message', msg => {
   }
 });
 
-const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
-const coolDown = new Set();
+const moment = ('moment');
 
-client.on('message',async message => {
-    
-if(message.author.bot) return;
-if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: 50
-};
+ 
 
-let userData = credits[message.author.id];
-let m = userData.credits;
+ 
+    client.on('message', message => {
+ 
+     
+        // Example: Fetching Balance
+        if (message.content.toUpperCase() === `${prefix}credits`) {
+ 
+            client.fetchBal(message.author.id).then((i) => {
+                message.channel.send(`**Balance:** ${i.money}`);
+            })
+ 
+ 
+        }
+ 
+        if (message.content.toUpperCase() === `${prefix}payyou`) {
 
-fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
-    if (err) console.error(err);
-  });
-  credits[message.author.id] = {
-      credits: m + 0.5,
-  }
-  
-    if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
-message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`);
-}
-});
+            client.updateBal(message.author.id, 1000000) //.then((i) => { // money.updateBal grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
+                 message.channel.send(`**You got $1000000!**\n**New Balance:** ${i.money}`);
+            
+ 
+        }
+ 
+        if (message.content.toUpperCase() === `${prefix}payfine`) {
+ 
+            client.updateBal(message.author.id, -500).then((i) => { // Since the 'value' is -500, it will 'add' -500, making the bal $500 lower.
+                message.channel.send(`**You paid your fine of $500!**\n**New Balance:** ${i.money}`);
+            })
+ 
+        }
+ 
+        if (message.content.toUpperCase() === prefix + `daily`) {
 
-client.on('message', async message => {
-    let amount = 250;
-    if(message.content.startsWith(prefix + "daily")) {
-    if(message.author.bot) return;
-    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
-    
-    let userData = credits[message.author.id];
-    let m = userData.credits + amount;
-    credits[message.author.id] = {
-    credits: m
-    };
-
-    fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => {
-    if (err) console.error(err);
-    });
-    
-    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
-        coolDown.add(message.author.id);
-    });
-    
-    setTimeout(() => {
-       coolDown.remove(message.author.id);
-    },86400000);
-    }
-
-    if (message.content.toUpperCase() === `${prefix}payyou`) {
-
-        money.updateBal(message.author.id, 1000000) //.then((i) => { // money.updateBal grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
-             message.channel.send(`**You got $1000000!**\n**New Balance:** ${i.money}`);
-        
-
-    }
-
-    if (message.content.toUpperCase() === `${prefix}payfine`) {
-
-        money.updateBal(message.author.id, -500).then((i) => { // Since the 'value' is -500, it will 'add' -500, making the bal $500 lower.
-            message.channel.send(`**You paid your fine of $500!**\n**New Balance:** ${i.money}`);
-        })
-
-    }
-});
+                if (client[message.author.username + message.guild.name] != moment().format('L')) {
+                    client[message.author.username + message.guild.name] = moment().format('L')
+                    client.updateBal(message.author.id, 500 ,200).then((i) => { // The daily ends of the day, so everyday they can get a daily bonus, if they missed it, they can't get it back again.
+                        message.channel.send({embed: {
+                            color: 3447003,
+                            description: 'Recieved your **$500** \`^daily`\. I think you should check \`^credits\`.',
+                            author: {
+                                name: `${message.author.username}#${message.author.discriminator}`,
+                                icon_url: message.author.avatarURL 
+                            }
+                        }});
+                    })
+                } else {
+                    message.channel.send({embed: {
+                        color: 3447003,
+                        description: 'You already recieved your \`^daily`\. Check later **' + moment().endOf('day').fromNow() + '**.', // When you got your daily already, this message will show up.
+                        author: {
+                            name: `${message.author.username}#${message.author.discriminator}`,
+                            icon_url: message.author.avatarURL 
+                        }
+                    }});
+                }
+            }
+          });
 
 client.login(process.env.BOT_TOKEN);
